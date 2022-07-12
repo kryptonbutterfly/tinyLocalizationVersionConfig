fdca11ea2b6da5aaf4cd795e2ac489c0187a3067
tinycodecrank <tinycodecrank@gmail.com>
2022-07-13 00:50:42 +0200

    add module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module de.tinycodecrank.LocalizationVersionConfig
 {
 	exports de.tinycodecrank.i18n.config;
 	
+	opens de.tinycodecrank.i18n.config to de.tinycodecrank.xmlConfig4J;
+	
 	requires de.tinycodecrank.xmlConfig4J;
 	requires java.xml;
 }
